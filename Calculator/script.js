95507da9ea623ab612657eb5eb2643b5e2fd5120
Sarah Manon Pradel <sapra18@student.sdu.dk>
2021-03-04 09:29:02 +0100

    Added Calculator Project
    Small project made with the intent of being used in the Code Café that I am hosting on behalf of SDU

---
+++ Calculator/script.js
@@ -0,0 +1,39 @@
+function calc10(value){
+    let result = Math.pow(10, eval(value));
+    return result;
+}
+
+function calcLog(value){
+    let result = Math.log10(eval(value));
+    return result;
+}
+
+function calcLn(value){
+    let result = Math.log(2.718, eval(value));
+    return result;
+}
+
+function calcRoot(value){
+    let result = Math.sqrt(eval(value));
+    return result;
+}
+
+function calcSquare(value){
+    let result = Math.pow(eval(value), 2);
+    return result;
+}
+
+function calcSin(value){
+    let result = Math.sin(eval(value));
+    return result;
+}
+
+function calcCos(value){
+    let result = Math.cos(eval(value));
+    return result;
+}
+
+function calcTan(value){
+    let result = Math.tan(eval(value));
+    return result;
+}
